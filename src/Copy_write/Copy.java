379aceab871622a0101f5b047520f228d7e7ec5e
tontan.mok38 <tontan.mok38@gmail.com>
2023-09-18 17:20:49 +0700

    Chang to try with resources

--- src/Copy_write/Copy.java
+++ src/Copy_write/Copy.java
@@ -6,25 +6,21 @@ import java.io.*;
 public class Copy {
 
     public static void copyFile(String source,String dest) throws IOException {
-        InputStream input = null;
-        OutputStream output = null;
-        try {
 
-            input = new FileInputStream(source);
-            output = new FileOutputStream(dest);
+        try (FileInputStream in= new FileInputStream(source);FileOutputStream out = new FileOutputStream(dest)) {
             byte[] file = new byte[1024];
             int leng;
-            while ((leng = input.read(file)) > 0) {
-                output.write(file, 0, leng);
+            while ((leng = in.read(file)) > 0) {
+                 out.write(file,0, leng);
             }
-        } finally {
-            input.close();
-            output.close();
+        }
+            finally {
+            System.out.println("Finish");
         }
     }
 
     public static void main(String[] args) throws IOException {
-        //copyFile(new File(/home/ploy/Downloads/Redy.txt"), new File("/home/ploy/Downloads/Big.txt"));
+      //  copyFile(new File("/home/ploy/Downloads/Redy.txt"), new File("/home/ploy/Downloads/Big.txt"));
         copyFile(args[0],args[1]);
     }
 }
